b83023d447ba6747349eb99674e3c390a60612df
ManOnPajamas <gustavo.deza@utec.edu.pe>
2019-05-22 20:31:56 -0500

    image2

--- producto.php
+++ producto.php
@@ -2,6 +2,9 @@
 require_once 'producto.entidad.php';
 require_once 'producto.model.php';
 
+ $db = mysqli_connect("localhost", "root", "", "image_upload");
+ $msg="";
+
 // Logica
 $alm = new Producto();
 $model = new ProductoModel();
@@ -43,6 +46,26 @@ if(isset($_REQUEST['action']))
 			break;
 	}
 }
+if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
+
+  	// image file directory
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM images");
 
 ?>
 
@@ -50,10 +73,41 @@ if(isset($_REQUEST['action']))
 <html lang="es">
 	<head>
 		<title>Producto</title>
+		<style type="text/css">
+			   #content{
+				width: 50%;
+				margin: 20px auto;
+				border: 1px solid #cbcbcb;
+			   }
+			   form{
+				width: 50%;
+				margin: 20px auto;
+			   }
+			   form div{
+				margin-top: 5px;
+			   }
+			   #img_div{
+				width: 80%;
+				padding: 5px;
+				margin: 15px auto;
+				border: 1px solid #cbcbcb;
+			   }
+			   #img_div:after{
+				content: "";
+				display: block;
+				clear: both;
+			   }
+			   img{
+				float: left;
+				margin: 5px;
+				width: 300px;
+				height: 140px;
+			   }
+</style>
         <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
 	</head>
     <body style="padding:15px;">
-
+		
         <div class="pure-g">
             <div class="pure-u-1-12">
                 
@@ -122,6 +176,34 @@ if(isset($_REQUEST['action']))
               
             </div>
         </div>
+		
+		<div id="content">
+		  <?php
+			while ($row = mysqli_fetch_array($result)) {
+			  echo "<div id='img_div'>";
+				echo "<img src='images/".$row['image']."' >";
+				echo "<p>".$row['image_text']."</p>";
+			  echo "</div>";
+			}
+		  ?>
+		  <form method="POST" action="index.php" enctype="multipart/form-data">
+			<input type="hidden" name="size" value="1000000">
+			<div>
+			  <input type="file" name="image">
+			</div>
+			<div>
+			  <textarea 
+				id="text" 
+				cols="40" 
+				rows="4" 
+				name="image_text" 
+				placeholder="Say something about this image..."></textarea>
+			</div>
+			<div>
+				<button type="submit" name="upload">POST</button>
+			</div>
+		  </form>
+		</div>
 
     </body>
 </html>
